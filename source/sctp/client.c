b0d9c0765aaa63fd3385960482c6e796469894e7
ozgurbagci <bagciozgur94@gmail.com>
2017-12-22 08:35:40 +0300

    Updates on SCTP

--- source/sctp/client.c
+++ source/sctp/client.c
@@ -6,57 +6,70 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stddef.h>
 #include <fcntl.h>
 #include <netdb.h>
 #include <unistd.h>
 
-int main(){
+int main(char args[], char filename[]) {
 
-  // создаем сокет
-  int sock;
-  if((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP)) == -1){
-    printf("Error create!\n");
-    return -1;
-  }
+	if (filename == "" || args == "") return -1;
 
-  // настройка канала
-  struct sctp_initmsg initmsg;
-  memset(&initmsg, 0, sizeof(initmsg));
-  initmsg.sinit_num_ostreams = 3; // выходные потоки
-  initmsg.sinit_max_instreams = 3; // входные потоки
-  initmsg.sinit_max_attempts = 2; // количество попыток
-  setsockopt(sock, IPPROTO_SCTP, SCTP_INITMSG, &initmsg, sizeof(initmsg));
+	int sock;
+	if((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP)) == -1){
+		return -1;
+	}
 
-  // структура для описания адресов сервера
-  struct addrinfo *res, hints;
-  hints.ai_family = AF_INET;
-  hints.ai_protocol = IPPROTO_SCTP;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_ADDRCONFIG | AI_V4MAPPED;
-  getaddrinfo("10.10.2.2", "2016", &hints, &res);
-  getaddrinfo("10.10.4.2", "2017", &hints, &res);
+	struct sctp_initmsg initmsg;
+	memset(&initmsg, 0, sizeof(initmsg));
+	initmsg.sinit_num_ostreams = 3;
+	initmsg.sinit_max_instreams = 3;
+	initmsg.sinit_max_attempts = 2;
+	setsockopt(sock, IPPROTO_SCTP, SCTP_INITMSG, &initmsg, sizeof(initmsg));
 
-  //
-  struct sockaddr_storage *connect = NULL;
-  int cc = 0, count = 0;
-  for(; res; res = res->ai_next){
-    connect = realloc(connect, cc + res->ai_addrlen);
-    memcpy((char*)connect + cc, res->ai_addr, res->ai_addrlen);
-    count++;
-    cc += res->ai_addrlen;
-  }
-  // соединение с сервером
-  if(sctp_connectx(sock, (struct sockaddr*)connect, count, 0) == -1){ 
-    perror("Error sctp_connectx");
-    exit(-1);
-  }
+	struct addrinfo *res, hints;
+	hints.ai_family = AF_INET;
+	hints.ai_protocol = IPPROTO_SCTP;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_ADDRCONFIG | AI_V4MAPPED;
+	char * running = strdupa(args);
+	while (running != "")
+	{
+		char * token = strsep(&running, ";");
+		getaddrinfo(token, "1234", &hints, &res);
+	}
 
-  // отправка и прием сообщения
-  while(1){
-    char buf[512] = "Hello!";
-    sctp_sendmsg(sock, (void*)buf, (size_t)strlen(buf), NULL, 0, 0, 0, 0, 0, 0); // 3й сконца - номер потока
-    sleep(2);
-  }
-  close(sock);
-  return 0;
+	struct sockaddr_storage *connect = NULL;
+	int cc = 0, count = 0;
+	for(; res; res = res->ai_next){
+		connect = realloc(connect, cc + res->ai_addrlen);
+		memcpy((char*)connect + cc, res->ai_addr, res->ai_addrlen);
+		count++;
+		cc += res->ai_addrlen;
+	}
+	if(sctp_connectx(sock, (struct sockaddr*)connect, count, 0) == -1){ 
+		exit(-1);
+	}
+	char *myfilename = filename;
+	char * cursor = strdupa(filename);
+	while (cursor != "")
+	{
+		myfilename = strsep(&running, "/");
+	}
+	sctp_sendmsg(sock, (void*)myfilename, (size_t)strlen(myfilename), NULL, 0, 0, 0, 0, 0, 0);
+	unsigned char buffer[512];
+	size_t bytesRead = 0;
+	FILE *myfile = fopen(filename, "rb");
+	if(myfile != NULL)
+	{
+		while((bytesRead = fread(buffer, sizeof(buffer), 1, myfile)) > 0)
+		{
+			sctp_sendmsg(sock, (void*)buffer, (size_t)strlen(buffer), NULL, 0, 0, 0, 0, 0, 0);
+		}
+	}
+	fclose(myfile);
+	char endmessage = EOF;
+	sctp_sendmsg(sock, (void*)endmessage, (size_t)strlen(endmessage), NULL, 0, 0, 0, 0, 0, 0);
+	close(sock);
+	return 0;
 }
--- source/sctp/server.c
+++ source/sctp/server.c
@@ -10,69 +10,68 @@
 #include <netdb.h>
 #include <unistd.h>
 
-int main(){
-  struct sockaddr_in client;
-  int sock;
-  // создание сокета
-  if((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP)) == -1){
-    printf("Error create socket!\n");
-    return -1;
-  }
+int main(char args[]){
+	struct sockaddr_in client;
+	int sock;
+	if((sock = socket(AF_INET, SOCK_STREAM, IPPROTO_SCTP)) == -1){
+		return -1;
+	}
 
-  // настройка канала
-  struct sctp_initmsg initmsg;
-  initmsg.sinit_num_ostreams = 3; // входные потоки
-  initmsg.sinit_max_instreams = 3; // выходные потоки
-  initmsg.sinit_max_attempts = 2; // максимальное количество попыток
-  setsockopt(sock, IPPROTO_SCTP, SCTP_INITMSG, &initmsg, sizeof(initmsg));
+	struct sctp_initmsg initmsg;
+	initmsg.sinit_num_ostreams = 3;
+	initmsg.sinit_max_instreams = 3;
+	initmsg.sinit_max_attempts = 2;
+	setsockopt(sock, IPPROTO_SCTP, SCTP_INITMSG, &initmsg, sizeof(initmsg));
 
-  // структура для описания адресов сервера
-  struct addrinfo *res, hints;
-  hints.ai_family = AF_INET;
-  hints.ai_protocol = IPPROTO_SCTP;
-  hints.ai_socktype = SOCK_STREAM;
-  hints.ai_flags = AI_ADDRCONFIG | AI_V4MAPPED;
-  getaddrinfo("10.10.2.2", "2016", &hints, &res);
-  getaddrinfo("10.10.4.2", "2017", &hints, &res);
+	struct addrinfo *res, hints;
+	hints.ai_family = AF_INET;
+	hints.ai_protocol = IPPROTO_SCTP;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_ADDRCONFIG | AI_V4MAPPED;
+	char * running = strdupa(args);
+	while (running != "")
+	{
+		char * token = strsep(&running, ";");
+		getaddrinfo(token, "1234", &hints, &res);
+	}
 
-  // 
-  struct sockaddr_storage *connect = NULL;
-  int cc = 0, count = 0;
-  for(; res; res = res->ai_next){
-    connect = realloc(connect, cc + res->ai_addrlen);
-    memcpy((char*)connect + cc, res->ai_addr, res->ai_addrlen);
-    count++;
-    cc += res->ai_addrlen;
-  }
-  // связка
-  if(sctp_bindx(sock, (struct sockaddr*)connect, count, SCTP_BINDX_ADD_ADDR) == -1){
-    perror("Error stcp_bindx");
-    return -1;
-  }
-
-  // объявляем очередь
-  if(listen(sock,5)==-1){
-    printf("Error listen!\n");
-    return -1;
-  }
-  int newsock;
-  char buffer[512];
-  int clnlen = sizeof(client), flags;
-  if((newsock = accept(sock, (struct sockaddr*)&client, &clnlen)) == -1){ // принимаем соединение от клиента
-    printf("Error accept!\n");
-    return -1;
-  }
-  printf("Новый клиент!\n");
-  while(1){
-    bzero((void*)&buffer, sizeof(buffer));
-    struct sctp_sndrcvinfo sndrcvinfo; // информация о пересылке
-    if(sctp_recvmsg(newsock, (void*)buffer, sizeof(buffer), (struct sockaddr*)NULL, 0, &sndrcvinfo, &flags) == -1){ // принимаем сообщение от клиента
-      perror("Error sctp_recvmsg!");
-      return -1;
-    }
-    printf(">>%s\n",buffer);
-  }
-  close(newsock); // закрываем связь с клиентом
-  close(sock); // закрываем сокет сервера
-  return 0;
+	struct sockaddr_storage *connect = NULL;
+	int cc = 0, count = 0;
+	for(; res; res = res->ai_next){
+		connect = realloc(connect, cc + res->ai_addrlen);
+		memcpy((char*)connect + cc, res->ai_addr, res->ai_addrlen);
+		count++;
+		cc += res->ai_addrlen;
+	}
+	if(sctp_bindx(sock, (struct sockaddr*)connect, count, SCTP_BINDX_ADD_ADDR) == -1){
+		return -1;
+	}
+	if(listen(sock,5)==-1){
+		return -1;
+	}
+	int newsock;
+	char buffer[512];
+	int clnlen = sizeof(client), flags;
+	if((newsock = accept(sock, (struct sockaddr*)&client, &clnlen)) == -1){
+		return -1;
+	}
+	bzero((void*)&buffer, sizeof(buffer));
+	struct sctp_sndrcvinfo sndrcvinf;
+	if (sctp_recvmsg(newsock, (void*)buffer, sizeof(buffer), (struct sockaddr*)NULL, 0, &sndrcvinf, &flags) == -1) {
+		return -1;
+	}
+	FILE *myfile = fopen(buffer, "ab+");
+	while(1){
+		bzero((void*)&buffer, sizeof(buffer));
+		struct sctp_sndrcvinfo sndrcvinfo;
+		if(sctp_recvmsg(newsock, (void*)buffer, sizeof(buffer), (struct sockaddr*)NULL, 0, &sndrcvinfo, &flags) == -1){
+			return -1;
+		}
+		fwrite(&buffer, sizeof(buffer), 1, myfile);
+		if (buffer[strlen(buffer) - 1] == EOF) break;
+	}
+	fclose(myfile);
+	close(newsock);
+	close(sock);
+	return 0;
 }
